83d0a878edfeea5d830f65b7330ea59235c5c68f
Mario Toshev <mario.a.toshev@gmail.com>
2023-06-14 16:14:52 +0200

    test properties update

--- src/test/java/com/example/easywheelz/IntegrationTests/CarControllerIntegrationTests.java
+++ src/test/java/com/example/easywheelz/IntegrationTests/CarControllerIntegrationTests.java
@@ -39,7 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CarControllerIntegrationTests {
+ class CarControllerIntegrationTests {
 
 
     @Autowired
--- src/test/java/com/example/easywheelz/IntegrationTests/UserIntegrationTests.java
+++
@@ -1,4 +0,0 @@
-package com.example.easywheelz.IntegrationTests;
-
-public class UserIntegrationTests {
-}
